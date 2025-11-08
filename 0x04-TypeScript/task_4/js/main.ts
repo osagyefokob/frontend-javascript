d55c28fa409e12ac1679a929203cfde47f14cf92
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-08 12:20:34 +0000

    Update main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,3 +1,7 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
 export const cpp = new Subjects.Cpp();
 export const java = new Subjects.Java();
 export const react = new Subjects.React();
