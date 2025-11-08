6314d8f17d6132589cf8e38135a85fc1a02e1592
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-08 12:17:06 +0000

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,7 +2,7 @@ namespace Subjects {
   export class Subject {
     teacher: Teacher | null = null;
 
-    setTeacher(teacher: Teacher): void {
+    setTeacher(teacher:Teacher): void {
       this.teacher = teacher;
     }
   }
